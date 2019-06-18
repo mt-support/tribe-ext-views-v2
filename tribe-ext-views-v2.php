e072a6fd3620a40c0f6e99c271610e687f9eaddd
Gustavo Bordoni <bordoni.dev@gmail.com>
2019-06-17 22:43:23 -0400

    Update tribe-ext-views-v2.php

--- tribe-ext-views-v2.php
+++ tribe-ext-views-v2.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * Plugin Name:       Tribe Events Extension: Views V2
- * Plugin URI:        TBD
- * GitHub Plugin URI: https://github.com/mt-support/tribe-ext-views-v2
- * Description:       Enable View V2 Design
- * Version:           1.0.0
+ * Plugin URI:        https://theeventscalendar.com/extensions/---the-extension-article-url---/
+ * GitHub Plugin URI: https://github.com/mt-support/tribe-ext-extension-template
+ * Description:       [Extension Description]
+ * Version:           1.0.1
+ * Extension Class:   Tribe\Extensions\Example\Main
  * Author:            Modern Tribe, Inc.
  * Author URI:        http://m.tri.be/1971
  * License:           GPL version 3 or any later version
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:       tribe-ext-views-v2
+ * Text Domain:       tribe-ext-extension-template
  *
  *     This plugin is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
@@ -25,16 +26,11 @@
 add_filter( 'tribe_display_settings_tab_fields', 'tribe_ext_views_v2_filter_settings_fields', 25 );
 
 function tribe_ext_views_v2_filter_settings_fields( $settings ) {
-	// only load on views v2
-	if ( ! class_exists( 'Tribe\Events\Views\V2\View' ) ) {
-		return $settings;
-	}
-
 	return Tribe__Main::array_insert_after_key(
 		'tribeEventsBasicSettingsTitle',
 		$settings,
 		[
-			Tribe\Events\Views\V2\View::$option_enabled => [
+			Tribe\Events\Views\V2\Manager::$option_enabled => [
 				'type'            => 'checkbox_bool',
 				'label'           => __( 'Enable View V2 Design', 'tribe-ext-views-v2' ),
 				'tooltip'         => __( 'Check this to use the new Shiny View Design!.', 'tribe-ext-views-v2' ),
